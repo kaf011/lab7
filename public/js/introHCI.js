a2614c051a5d7c463ca0f2d1c3e04ff2948b2c3d
Kaiyun Fu <kaf011@ucsd.edu>
2020-02-20 14:24:03 -0800

    1

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,12 +2,19 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+  initializePage();
 });
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+  $(".likeBtn").click(likeBtnClicked);
+  $(".btn btn-default likeBtn").click(likeBtnClicked);
+  // your code here
+}
+
+function likeBtnClicked() {
+  console.log("Clicked");
+  ga("send", "event", "like", "click");
 }
